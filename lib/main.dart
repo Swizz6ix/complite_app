0ea746ebd04977ecabb7f38a3a83cb5e8e9942fd
unknown <ferdinandcharles6@gmail.com>
2026-04-02 14:15:38 +0100

    updated main to run simple testing screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:complite/ui/company_list_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logging/logging.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  // initFileLogger('compile');
+  // final logger = Logger('compile-app');
+  // logger.info('app start');
+
+  runApp(
+    ProviderScope(
+      // overrides: [],
+      // observers: [ProviderLogger()],
+      child: const MainApp()
+    )
+    );
 }
 
 class MainApp extends StatelessWidget {
@@ -10,10 +24,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: true,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: CompanyListPage(),
       ),
     );
   }
